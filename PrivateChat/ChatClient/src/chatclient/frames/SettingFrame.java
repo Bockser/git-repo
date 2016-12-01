a537dac2fbe88c374d2ef6cf096a6a8de09fdd91
Bockser <kappabockser@gmail.com>
2016-12-01 15:32:51 +0500

    Создано окно настройки сервера

---
+++ PrivateChat/ChatClient/src/chatclient/frames/SettingFrame.java
@@ -0,0 +1,18 @@
+package chatclient.frames;
+
+import javax.swing.*;
+
+import static chatclient.helpers.ChatConstans.*;
+
+/**
+ * Created by Администратор on 01.12.2016.
+ */
+public class SettingFrame extends AbstractFrame{
+
+    private static final String FRAME_TITLE = "Setting";
+    public SettingFrame(FrameManager manager) {
+        super(manager, MAIN_FRAME_ID);
+        setTitle(FRAME_TITLE);
+        setVisible(false);
+    }
+}
