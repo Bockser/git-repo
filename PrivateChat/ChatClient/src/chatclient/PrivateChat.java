d26586121b5425b5092dbbace0591453e6104c14
Bockser <kappabockser@gmail.com>
2016-12-02 22:22:43 +0500

    перенос директории. подключение к серверу и отправка сообщения

--- PrivateChat/ChatClient/src/chatclient/PrivateChat.java
+++ PrivateChat/ChatClient/src/chatclient/PrivateChat.java
@@ -5,6 +5,8 @@ import chatclient.frames.MainFrame;
 import sun.applet.Main;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
 import java.net.Socket;
 
 /**
@@ -12,13 +14,26 @@ import java.net.Socket;
  */
 public class PrivateChat implements FrameManager{
 
+
     private MainFrame mainFrame;
     public PrivateChat () {
         mainFrame = new MainFrame(this);
+
+        Socket s = null;
         try {
-            Socket s = new Socket("localhost", 12321);
+            s = new Socket("localhost", 12321);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            out.println("my message");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        finally {
+            try {
+                if (s != null)
+                    s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     public static void main(String[] args) {
