a686af857ea4aa883a02d21073dd661084a831f7
Bockser <kappabockser@gmail.com>
2016-12-03 16:06:42 +0500

    изменен смысл интерфейса

--- PrivateChat/ChatServer/src/chatserver/frames/ServerManager.java
+++ PrivateChat/ChatServer/src/chatserver/frames/ServerManager.java
@@ -3,13 +3,11 @@ package chatserver.frames;
 /**
  * Created by Администратор on 01.12.2016.
  */
-public interface FrameManager {
+public interface ServerManager {
 
     void hideFrame(int frameID);
 
     void showFrame(int frameID);
-
-    AbstractFrame getFrame(int frameID);
 
     void exit();
 
