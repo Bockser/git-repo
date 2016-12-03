56f2e18e7dacd3e6ed9d6701e75a8c2d514f453f
Bockser <kappabockser@gmail.com>
2016-12-03 16:08:16 +0500

    реализация потока сервера переложена на главный класс

--- PrivateChat/ChatServer/src/chatserver/PrivateChatServer.java
+++ PrivateChat/ChatServer/src/chatserver/PrivateChatServer.java
@@ -1,18 +1,16 @@
 package chatserver;
 
-
-import chatserver.frames.AbstractFrame;
-import chatserver.frames.FrameManager;
 import chatserver.frames.MainFrame;
+import chatserver.frames.ServerManager;
 import chatserver.frames.SettingFrame;
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.ArrayList;
+import java.util.*;
 import java.io.InputStreamReader;
 
 import static chatserver.helpers.ChatConstans.*;
@@ -20,23 +18,63 @@ import static chatserver.helpers.ChatConstans.*;
 /**
  * Created by Bockser on 30.11.2016.
  */
-public class PrivateChatServer implements FrameManager{
-
-
+public class PrivateChatServer implements ServerManager, Runnable{
 
     private MainFrame mainFrame;
     private SettingFrame settingFrame;
-    private ConnectionListener connectionListener;
+
     private int serverPort = DEFAULT_PORT;
+    private boolean doRun = false;
+
+    private ServerSocket server = null;
+    private List<Connection> connections = Collections.synchronizedList(new ArrayList<Connection>());
 
     public static void main(String[] args) {
-       new PrivateChatServer();
+        new PrivateChatServer();
     }
 
     private PrivateChatServer() {
-        mainFrame = new MainFrame(this);
-        settingFrame = new SettingFrame(this);
+        MainFrame.initInstance(this);
+        mainFrame = MainFrame.getInstance();
+
+        SettingFrame.initInstance(this);
+        settingFrame = SettingFrame.getInstance();
+
         showFrame(MAIN_FRAME_ID);
+    }
+
+    @Override
+    public void run() {
+        int connectionCount = 0;
+        doRun = true;
+        try {
+            server = new ServerSocket(serverPort);
+            server.setSoTimeout(3000);
+            mainFrame.addLog("server start");
+            while (doRun == true) {
+                try {
+                    Connection connection = new Connection(connectionCount, server.accept());
+                    connections.add(connection);
+                    connection.start();
+                    connectionCount++;
+                } catch (SocketTimeoutException e) {}
+            }
+            mainFrame.addLog("Server stopped");
+        } catch (IOException e) {
+            mainFrame.addLog("Error: ServerSocket was not open");
+        } finally { closeAll(); }
+    }
+
+    private void closeAll() {
+        try {
+            server.close();
+            synchronized (connections) {
+                Iterator<Connection> iterator = connections.iterator();
+                while (iterator.hasNext())
+                    iterator.next().close();
+            }
+        } catch (IOException e) {
+            mainFrame.addLog("Error: Connection was not closed");        }
     }
 
     @Override
@@ -68,102 +106,66 @@ public class PrivateChatServer implements FrameManager{
     }
 
     @Override
-    public AbstractFrame getFrame(int frameID) {
-        switch (frameID) {
-            case MAIN_FRAME_ID:
-                return mainFrame;
-            case SETTING_FRAME_ID:
-                return settingFrame;
-            default:
-                return null;
+    public void exit() {
+        if (doRun) {
+            mainFrame.addLog("Error: Server is already running!");
+        } else {
+            settingFrame.dispose();
+            mainFrame.dispose();
         }
     }
 
     @Override
-    public void exit() {
-        settingFrame.dispose();
-        mainFrame.dispose();
-    }
-
-    @Override
     public void startServer() {
-        mainFrame.addLog("Trying to start the server");
-        connectionListener = new ConnectionListener();
-        connectionListener.start();
+        if (doRun == true) mainFrame.addLog("Error: Server is already running");
+        else {
+            mainFrame.addLog("Trying to start the server");
+            new Thread(this).start();
+        }
     }
 
     @Override
     public void stopServer() {
         mainFrame.addLog("Stopping server");
-        connectionListener.stopServer();
+        doRun = false;
 
     }
-    public class ConnectionListener extends Thread{
 
-        private boolean doRun = true;
-        private ServerSocket server;
-        private ArrayList<ConnectionHandler> listOfConnection = new ArrayList();
-        private ConnectionListener() {
+
+    class Connection extends Thread{
+
+        final int sessionID;
+        Socket connection;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public Connection(final int sessionID, Socket connection) {
+            this.sessionID = sessionID;
+            this.connection = connection;
+            try {
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                out = new PrintWriter(connection.getOutputStream(), true);
+            } catch (IOException e) { e.printStackTrace(); }
             setDaemon(true);
         }
 
         @Override
         public void run() {
-            int connectionCount = 0;
-            try {
-                server = new ServerSocket(serverPort);
-                server.setSoTimeout(3000);
-                mainFrame.addLog("server start");
-                while (doRun == true) {
-                    try {
-                        listOfConnection.add(new ConnectionHandler(connectionCount, server.accept()));
-                        connectionCount++;
-                    } catch (SocketTimeoutException e) {
+            String[] info = {Integer.toString(sessionID), "unsupported", connection.getInetAddress().toString(), "0"};
+            mainFrame.addLog("New connection");
 
-                    }
-                }
-                server.close();
-                mainFrame.addLog("Server stopped");
-            } catch (IOException e) {
-                mainFrame.addLog("ServerSocket was not open");
-            }
+            mainFrame.addConnectionInfo(info);
+        }
+
+        public void close() {
             try {
-                server.close();
+                connection.close();
+                in.close();
+                out.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        public void stopServer () {
-            doRun = false;
-        }
-
-        class ConnectionHandler extends Thread{
-            final int sessionID;
-            Socket connection;
-
-            public ConnectionHandler(final int sessionID, Socket connection) {
-                this.sessionID = sessionID;
-                this.connection = connection;
-                setDaemon(true);
-                this.start();
-            }
-
-            @Override
-            public void run() {
-                String[] info = {Integer.toString(sessionID), "unsupported", connection.getInetAddress().toString(), "0"};
-                mainFrame.addLog("New connection");
-
-                try {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    mainFrame.addLog(in.readLine());
-                } catch (IOException e) {
-
-                    e.printStackTrace();
-                }
-                mainFrame.addConnectionInfo(info);
-            }
-
-        }
     }
 }
