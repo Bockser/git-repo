3d5866121e93fa95e6d76fe0dd9f035bcb4c4d4e
Bockser <kappabockser@gmail.com>
2016-12-04 16:46:17 +0500

    работа чата вынесена в отдельный поток

--- PrivateChat/ChatClient/src/chatclient/PrivateChat.java
+++ PrivateChat/ChatClient/src/chatclient/PrivateChat.java
@@ -1,29 +1,47 @@
 package chatclient;
 
-import chatclient.frames.FrameManager;
+import chatclient.frames.ChatClient;
+import chatclient.frames.LogonFrame;
 import chatclient.frames.MainFrame;
-import sun.applet.Main;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.Socket;
 
 /**
  * Created by Администратор on 02.12.2016.
  */
-public class PrivateChat implements FrameManager{
+public class PrivateChat implements ChatClient, Runnable{
 
 
     private MainFrame mainFrame;
+    private LogonFrame logonFrame;
+    private InetAddress host;
+    private String name = "unname";
+
     public PrivateChat () {
         mainFrame = new MainFrame(this);
+        logonFrame = new LogonFrame(this);
 
+    }
+
+    public static void main(String[] args) {
+        new PrivateChat();
+    }
+
+    public void connect() {
+        Thread client = new Thread(this);
+        client.start();
+    }
+
+    @Override
+    public void run() {
         Socket s = null;
         try {
-            s = new Socket("localhost", 12321);
+            s = new Socket(host, 12321);
             PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-            out.println("my message");
+            out.println("AUTH");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,7 +54,12 @@ public class PrivateChat implements FrameManager{
             }
         }
     }
-    public static void main(String[] args) {
-        new PrivateChat();
+
+    @Override
+    public void connect(InetAddress host, String username) {
+        this.host = host;
+        this.name = username;
+        Thread chat = new Thread(this);
+        chat.start();
     }
 }
