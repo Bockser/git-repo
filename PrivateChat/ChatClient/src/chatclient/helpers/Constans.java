89dc1f6cad4f8d01bf1591e9f8202d8d333a40a6
Bockser <kappabockser@gmail.com>
2016-12-04 22:02:34 +0500

    создан класс с константами

---
+++ PrivateChat/ChatClient/src/chatclient/helpers/Constans.java
@@ -0,0 +1,18 @@
+package chatclient.helpers;
+
+/**
+ * Created by Bockser on 04.12.2016.
+ */
+public final class Constans {
+
+    private Constans() {}
+
+    //SERVER COMMAND
+    public static final String AUTH_COMMAND = "AUTH";
+    public static final String REG_COMMAND = "REG";
+    public static final String EXIT_COMMAND = "EXIT";
+
+    //GENERAL COMMAND
+    public static final String PUBLIC_MESSAGE = "PUBLIC";
+    public static final String PRIVATE_MESSAGE = "PRIVATE";
+}
