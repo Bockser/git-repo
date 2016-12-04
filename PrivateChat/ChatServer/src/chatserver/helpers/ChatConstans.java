e6f657f2c9a652ddb164e4a101902954857822e0
Bockser <kappabockser@gmail.com>
2016-12-04 22:05:25 +0500

    добавлены константы команд команд

--- PrivateChat/ChatServer/src/chatserver/helpers/ChatConstans.java
+++ PrivateChat/ChatServer/src/chatserver/helpers/ChatConstans.java
@@ -15,5 +15,10 @@ public final class ChatConstans {
     //SERVER COMMAND
     public static final String AUTH_COMMAND = "AUTH";
     public static final String REG_COMMAND = "REG";
+    public static final String EXIT_COMMAND = "EXIT";
+
+    //GENERAL COMMAND
+    public static final String PUBLIC_MESSAGE = "PUBLIC";
+    public static final String PRIVATE_MESSAGE = "PRIVATE";
 
 }
