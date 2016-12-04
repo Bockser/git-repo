5dd5837f8caa9396b9443899f9874bcda1ddead8
Bockser <kappabockser@gmail.com>
2016-12-04 22:04:57 +0500

    добавлен метод приема сообщений

--- PrivateChat/ChatClient/src/chatclient/PrivateChat.java
+++ PrivateChat/ChatClient/src/chatclient/PrivateChat.java
@@ -1,9 +1,12 @@
 package chatclient;
 
+import static chatclient.helpers.Constans.*;
+
 import chatclient.frames.ChatClient;
 import chatclient.frames.LogonFrame;
 import chatclient.frames.MainFrame;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -17,8 +20,17 @@ public class PrivateChat implements ChatClient, Runnable{
 
     private MainFrame mainFrame;
     private LogonFrame logonFrame;
+
+
     private InetAddress host;
-    private String name = "unname";
+    private BufferedReader in;
+    private PrintWriter out;
+    private String name = "";
+    private boolean exitClient = false;
+
+
+
+
 
     public PrivateChat () {
         mainFrame = new MainFrame(this);
@@ -40,15 +52,21 @@ public class PrivateChat implements ChatClient, Runnable{
         Socket s = null;
         try {
             s = new Socket(host, 12321);
-            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-            out.println("AUTH");
+            out = new PrintWriter(s.getOutputStream(), true);
+            System.out.println(AUTH_COMMAND + "|" + name);
+            out.println(AUTH_COMMAND + "|" + name);
+            while (!exitClient) {}
         } catch (IOException e) {
-            e.printStackTrace();
+            logonFrame.showFrame(true);
+            mainFrame.setVisible(false);
         }
         finally {
             try {
-                if (s != null)
+                if (s != null) {
+                    out.println(EXIT_COMMAND);
                     s.close();
+
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -59,7 +77,42 @@ public class PrivateChat implements ChatClient, Runnable{
     public void connect(InetAddress host, String username) {
         this.host = host;
         this.name = username;
+        logonFrame.setVisible(false);
+        mainFrame.setVisible(true);
         Thread chat = new Thread(this);
         chat.start();
+    }
+
+    private class Resender extends Thread {
+
+        private boolean stop = false;
+
+        public void setStop(boolean stop) {
+            this.stop = stop;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                while (!stop) {
+                    String[] command = in.readLine().split("|");
+                    switch (command[0]) {
+                        case PUBLIC_MESSAGE:
+                            mainFrame.showPublicMessage(command[1], command[2]);
+                            break;
+                        case PRIVATE_MESSAGE:
+                            mainFrame.showPrivateMessage(command[1], command[2]);
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
