35cdb994b219517e056d2fdfa1dc04c93252428b
Bockser <kappabockser@gmail.com>
2016-12-06 15:22:37 +0500

    разработка ГУИ главного окна начата сначала

--- PrivateChat/ChatClient/src/chatclient/frames/MainFrame.java
+++ PrivateChat/ChatClient/src/chatclient/frames/MainFrame.java
@@ -6,6 +6,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
 
 /**
  * Created by Администратор on 02.12.2016.
@@ -13,6 +14,13 @@ import java.awt.event.WindowListener;
 public class MainFrame extends JFrame {
 
     private ChatClient chatClient;
+
+    private JList listOnlineUser;
+    private JTabbedPane messageTabbedPane;
+    private ArrayList<JPanel> messagePanels;
+
+    private GridBagConstraints c;
+
 
     public MainFrame(ChatClient chatClient) {
         this.chatClient = chatClient;
@@ -22,64 +30,33 @@ public class MainFrame extends JFrame {
         setVisible(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        //panel.setSize(100,100);
-        panel.setBorder(new EmptyBorder(20, 20,20, 20));
-        //add(BorderLayout.SOUTH, panel);
-        JTabbedPane messagePanels = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
-        //messagePanels.setBorder(new EmptyBorder(20, 20,20, 20));
-        JPanel publicMessages = new JPanel();
-        JTextArea publicMessagesArea = new JTextArea();
-        publicMessages.add(BorderLayout.CENTER, publicMessagesArea);
-        messagePanels.add("Public chat", publicMessages);
-        add(BorderLayout.CENTER, messagePanels);
+        setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        initListUser();
+        initMessageTabbedPane();
 
 
-        JPanel panelUsers = new JPanel();
-        //panelUsers.setBorder(new EmptyBorder(20, 20,20, 20));
-        add(BorderLayout.EAST, panelUsers);
-
-        String[] data = {"element 1", "element 2", "element 3"};
-        JList listUsers = new JList(data);
-        listUsers.setBounds(0, 0, 200 , 200);
-        listUsers.setFixedCellWidth(200);
-        panelUsers.add(BorderLayout.CENTER, listUsers);
-
-
-
-        /*JPanel panel = new JPanel();
-        panel.setSize(300,500);
-
-        JList list = new JList(data);
-        list.setLayoutOrientation(JList.VERTICAL);
-        list.setSize(200,500);
-
-        add(BorderLayout.EAST, list);
-        add(BorderLayout.WEST, panel);
-
-        panel.setLayout(new BorderLayout());
-
-        JTextArea ta1 = new JTextArea("123213");
-        JTextArea ta2 = new JTextArea("qweqweqwe");
-        ta1.setSize(300, 250);
-        ta1.setSize(300, 250);
-
-        panel.add(BorderLayout.NORTH, ta1);
-        panel.add(BorderLayout.SOUTH, ta2);*/
-
-        //pack();
     }
 
     private void initListUser() {
+        listOnlineUser = new JList(new String[]{"123", "123", "123"});
 
-
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 3;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 5;
+        add(listOnlineUser, c);
     }
 
-    public void showPublicMessage(String seller, String message) {
+    private void initMessageTabbedPane() {
+        messagePanels = new ArrayList<JPanel>();
+        messageTabbedPane = new JTabbedPane();
     }
 
-    public void showPrivateMessage(String seller, String message) {
+    public void displayPublicMessage(String seller, String message) {
     }
 
-
+    public void displayPrivateMessage(String name, String message) {
+    }
 }
