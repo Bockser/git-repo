903bd8054190d24d10d0eeb545a3dd8400a02aee
Bockser <kappabockser@gmail.com>
2016-12-07 16:10:56 +0500

    добавлен входящий поток, реализованы новые методы интерфейса, создан экзумпляр потока для приема сообщений

--- PrivateChat/ChatClient/src/chatclient/PrivateChat.java
+++ PrivateChat/ChatClient/src/chatclient/PrivateChat.java
@@ -8,6 +8,7 @@ import chatclient.frames.MainFrame;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -28,14 +29,11 @@ public class PrivateChat implements ChatClient, Runnable{
     private String name = "";
     private boolean exitClient = false;
 
-
-
-
-
     public PrivateChat () {
         mainFrame = new MainFrame(this);
         logonFrame = new LogonFrame(this);
-
+        //mainFrame.setVisible(true);
+        logonFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -47,9 +45,12 @@ public class PrivateChat implements ChatClient, Runnable{
         Socket s = null;
         try {
             s = new Socket(host, 12321);
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             out = new PrintWriter(s.getOutputStream(), true);
             out.println(AUTH_COMMAND);
             out.println(name);
+            Resender resender = new Resender();
+            resender.start();
             while (!exitClient) { }
         } catch (IOException e) {
             logonFrame.showFrame(true);
@@ -89,6 +90,11 @@ public class PrivateChat implements ChatClient, Runnable{
     }
 
     @Override
+    public String getMyName() {
+        return name;
+    }
+
+    @Override
     public void sendPublicMessage(String message) {
         out.println(PUBLIC_MESSAGE);
         out.println(name);
@@ -98,9 +104,13 @@ public class PrivateChat implements ChatClient, Runnable{
     @Override
     public void sendPrivateMessage(String addressee, String message) {
         out.println(PRIVATE_MESSAGE);
-        out.println(name);
         out.println(addressee);
         out.println(message);
+    }
+
+    @Override
+    public void sendCommand(String command, String[] args) {
+
     }
 
     private class Resender extends Thread {
